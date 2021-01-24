625cf2e14c9265432df198bf0699e0580d3ff149
Krish Nerkar <krishnerkar@gmail.com>
2021-01-24 11:31:40 +0530

    Added Auth

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ client.on("message", function(message) {
 }); 
 
 const url = "https://discord.com/api/v8/channels/801784356711956522/messages"
-const auth = ""
+// const auth = "NTY5ODI4MjkwMjUzMTYwNDQ5.YAarfw.hhhWAWBa9EjYT_Tarqs141CIKkA"
+const auth = "NTY5ODI4MjkwMjUzMTYwNDQ5.YA0KoQ.vCHzzrtBix5bjR3YFf_P3A6Lplg"
+             
 const headers = {"Authorization": auth,
                "Content-Type": 'application/json', 'referer': "https://discord.com/channels/801784356217421874/801784356711956522"}
 
@@ -27,6 +29,45 @@ const options = {
     headers : headers, 
     method : "POST"
 }    
+
+const login_url = "https://discord.com/api/v8/auth/login"
+
+const login_headers = {
+  "Content-Type" : "application/json", 
+  "referer" : "https://discord.com/login"
+ }
+
+ const login_options = {
+   headers : login_headers,
+   method : "POST"
+ }
+
+ const creds = JSON.stringify({
+   "login":"email",
+   "password":"pass",
+   "undelete":false,
+   "captcha_key":null,
+   "login_source":null,
+   "gift_code_sku_id":null
+ })
+
+const login_req = https.request(login_url, login_options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  // console.log(res)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+login_req.on('error', (error) => {
+  console.error(error)
+})
+
+login_req.write(creds)
+login_req.end()
+
+
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
@@ -60,6 +101,8 @@ req.on('error', (error) => {
 
 req.write(data)
 req.end()
+
+    // io.emit('chat message', msg);
   });
 });
 
